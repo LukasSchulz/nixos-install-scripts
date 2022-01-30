141433d4f28fe3fc65d7578e094771d5558652ae
LukasSchulz <allesmoeglicheundvielmehr@hotmail.de>
2022-01-30 21:37:17 +0100

    Update nixos-install-hetzner-cloud.sh

--- hosters/hetzner-cloud/nixos-install-hetzner-cloud.sh
+++ hosters/hetzner-cloud/nixos-install-hetzner-cloud.sh
@@ -66,7 +66,7 @@ echo '
 
   users.users.root.openssh.authorizedKeys.keys = [
     # Replace this by your SSH pubkey!
-    "ssh-rsa AAAAAAAAAAA..."
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN2SA6gC3tGLC5tsh6KN+UZ52wjPlgw5kAV2ZzHwatFS ausgeschwurbelt"
   ];
 }
 ' >> /mnt/etc/nixos/configuration.nix
